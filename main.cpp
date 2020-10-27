f94572f5bef874c327f4622f9d89a506b183b100
Hiuming <hieuminh282000@gmail.com>
2020-10-27 20:29:31 +0700

    Add constants and library

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <unistd.h>     // fork()
+#include <sys/wait.h>   // waitpid()
+#include <stdio.h>      // printf(), fgets()
+#include <string.h>     // strtok(), strcmp(), strdup()
+#include <stdlib.h>     // free()
+#include <fcntl.h>      // open(), creat(), close()
+
+using namespace std;
+const unsigned MAX_LINE_LENGTH = 100;
+const unsigned BUF_SIZE = 50;
+const unsigned REDIR_SIZE = 2;
+const unsigned PIPE_SIZE = 3;
+const unsigned MAX_HISTORY = 30;
+const unsigned MAX_COMMAND_NAME = 30;
