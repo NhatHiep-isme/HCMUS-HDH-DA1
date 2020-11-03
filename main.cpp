fe12664fea4aed82d40d4146b638542b001ab166
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2020-11-03 14:15:43 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,3 +12,20 @@ const unsigned REDIR_SIZE = 2;
 const unsigned PIPE_SIZE = 3;
 const unsigned MAX_HISTORY = 30;
 const unsigned MAX_COMMAND_NAME = 30;
+
+void ParseCommand(char cmdString[], char *argv[])
+{
+  for(int i=0; i< BUF_SIZE; i++)
+  {
+    argv[i] = NULL;
+  }
+  
+  int index = 0;
+  char *tempStr = strtok(cmdstring, " ");
+  while(tempStr != NULL)
+  {
+    argv[index++] = tempStr;
+    tempStr = strtok(NULL, " ");
+  }
+  argv[index] = NULL;
+}
