969b767a64a7dd3a7a70361cdebba6ee99da096f
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2020-11-04 17:00:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void ParseCommand(char *cmdString, char *argv[], int *waitFlag)
 {
   int index = 0;
   char delim = " ";
-  char *tempstr = strtok(cmdString, delim);
+  char *tempStr = strtok(cmdString, delim);
   while(tempStr!= NULL)
   {
     argv[index] = tempStr;
@@ -33,6 +33,61 @@ void ParseCommand(char *cmdString, char *argv[], int *waitFlag)
   else wait = 0;
 }
 
+void ChildProcess(char *argv[], char *redirectArgv[])
+{
+  int fdIn, fdOut;
+  if(redirectArgv[0] != NULL)
+  {
+    //Rediect output
+    if(strcmp(redirectArgv[0], ">")==0)
+    {
+      fdOut = creat(redirectArgv[1], O_WRONLY | O_CREAT | S_IRWXU);
+      if(fdOut == -1)
+      {
+        printf("Can't open file.\n");
+      }
+      
+      dup2(fdOut, STDOUT_FILENO);
+      if(dup2(fdOut, STDOUT_FILENO) < 0)
+      {
+        printf("Failed redirecting output.\n");
+        exit(2);
+      }
+      
+      if(close(fdOut) == -1)
+      {
+        printf("Failed closing output.\n");
+      }
+    }
+    
+    //Redirect input
+    if(strcmp(redirectArgv[0], "<")==0)
+    {
+      fdIn = open(redirectArgv[1], O_RDONLY);
+      if(fdIn == -1)
+      {
+        printf("Can't open file.\n");
+      }
+      
+      dup2(fdIn, STDIN_FILENO);
+      if(dup2(fdIn, STDIN_FILENO) < 0)
+      {
+        printf("Failed redirecting input.\n");
+        exit(2);
+      }
+      
+      if(close(fdIn) == -1)
+      {
+        printf("Failed closing input.\n");
+      }
+    }
+  }
+  
+  if(exexvp(argv[0], argv) == -1)
+  {
+    printf("Failed executing command!\n");
+  }
+}
 void SaveHistory(char *historyList[], int &historyIndex, char* command) //luu toan bo lich su command
 {
   if(historyCount < MAX_HISTORY_SIZE)
