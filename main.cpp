72519784fbfa3aa8d618025323341400ba9006fc
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2020-11-07 20:43:10 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // SimpleShell123.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <unistd.h>     // fork()
 #include <sys/wait.h>   // waitpid()
 #include <stdio.h>      // printf(), fgets()
@@ -45,7 +44,7 @@ void ParseCmd(char cmdString[], char *argv[], int *waitFlag) {
 }
 
 void SaveHistoryList(char *historyList[], int &historyIndex, char* cmdString) {
-	// Neu history_count vuot qua MAX_HISTORY_SIZE, hay ghi de lenh cuoi cung
+	// Neu history_count vu?t quá MAX_HISTORY_SIZE, hãy ghi dè l?nh cu?i cùng
 
 	if (historyIndex < MAX_HISTORY_SIZE) {
 		strcpy(historyList[historyIndex++], cmdString);
@@ -92,17 +91,17 @@ void ChildProcess(char* argv[], char* redirArgv[]) {
 		// Redirect output
 		if (strcmp(redirArgv[0], ">") == 0) {
 
-			// Lay file description
+			// Get file description
 			fdOut = creat(redirArgv[1], S_IRWXU);
 			if (fdOut == -1) {
 				printf("Redirect output failed");
 				exit(EXIT_FAILURE);
 			}
 
-			// Thay the stdout -> output
+			// Replace stdout with output file
 			dup2(fdOut, STDOUT_FILENO);
 
-			// Kiem tra loi ( khi dong chuong trinh)
+			// Check for error on close
 			if (close(fdOut) == -1) {
 				printf("Closing output failed");
 				exit(EXIT_FAILURE);
@@ -126,7 +125,7 @@ void ChildProcess(char* argv[], char* redirArgv[]) {
 		}
 	}
 
-	//Thuc thi lenh cua user nhap vao trong tien trinh con
+	// Execute user command in child process
 	if (execvp(argv[0], argv) == -1) {
 		printf("Fail to execute command");
 		exit(EXIT_FAILURE);
@@ -139,12 +138,13 @@ void ParentProcess(pid_t childPid, int waitFlag) {
 	printf("Parent <%d> spawned a child <%d>.\n", getpid(), childPid);
 	switch (waitFlag) {
 
-		// Hai tien trinh cha va con cung luc chay
+		// Parent and child are running concurrently
 	case 0: {
 		waitpid(childPid, &status, 0);
 		break;
+	}
 
-		// Tien trinh cha doi tien trinh con ( with PID) cham dut
+			// Parent waits for child process with PID to be terminated
 	default: {
 		waitpid(childPid, &status, WUNTRACED);
 
@@ -154,172 +154,170 @@ void ParentProcess(pid_t childPid, int waitFlag) {
 		break;
 	}
 	}
+}
+
+bool ParsePipe(char* argv[], char *childArgv1[], char *childArgv2[]) {
+	int index = 0, splitIndex = 0;
+	bool containsPipe = false;
+	int count = 0;
+
+	while (argv[index] != NULL) {
+
+		// Check if user_command contains pipe character |
+		if (strcmp(argv[index], "|") == 0) {
+			splitIndex = index;
+			containsPipe = true;
+		}
+		index++;
 	}
 
-	// Ham xu ly pipe
-	bool ParsePipe(char* argv[], char *childArgv1[], char *childArgv2[]) {
-		int index = 0, splitIndex = 0;
-		bool containsPipe = false;
-		int count = 0;
-
-		while (argv[index] != NULL) {
-
-			// Kiem tra xem user command co bao gom ki tu pipe "|"
-			if (strcmp(argv[index], "|") == 0) {
-				splitIndex = index;
-				containsPipe = true;
-			}
-			index++;
-		}
-
-		if (!containsPipe) {
-			return false;
-		}
-
-		//Copy cac tham so truoc khi tach pipe vao bien childArgv1[]
-		for (index = 0; index < splitIndex; index++) {
-			childArgv1[index] = strdup(argv[index]);
-		}
-		childArgv1[index++] = NULL;
-
-		// Copy cac tham so sau khi tach pipe vao bien childArgv2[]
-		while (argv[index] != NULL) {
-			childArgv2[index - splitIndex - 1] = strdup(argv[index]);
-			index++;
-		}
-		childArgv2[index - splitIndex - 1] = NULL;
-
-		return true;
+	if (!containsPipe) {
+		return false;
 	}
 
+	// Copy arguments before split pipe position to child01_argv[]
+	for (index = 0; index < splitIndex; index++) {
+		childArgv1[index] = strdup(argv[index]);
+	}
+	childArgv1[index++] = NULL;
+
+	// Copy arguments after split pipe position to child02_argv[]
+	while (argv[index] != NULL) {
+		childArgv2[index - splitIndex - 1] = strdup(argv[index]);
+		index++;
+	}
+	childArgv2[index - splitIndex - 1] = NULL;
+
+	return true;
+}
 
 
-	void ExecWithPipe(char* childArgv1[], char* childArgv2[]) {
-		int pipefd[2];
 
-		if (pipe(pipefd) == -1) {
-			/*
-					   Tao 1 pipe voi 1 input va 1 output (file descriptor)
-						  Index = 0 -> read pipe, Index = 1 -> write pipe
-			*/
-			printf("pipe() failed");
-			exit(EXIT_FAILURE);
-		}
+void ExecWithPipe(char* childArgv1[], char* childArgv2[]) {
+	int pipefd[2];
 
-		// Tao tien trinh con thu nhat (child 1)
-		if (fork() == 0) {
+	if (pipe(pipefd) == -1) {
+		/* Create a pipe with 1 input and 1 output file descriptor
+		Notation: Index = 0 ==> read pipe, Index = 1 ==> write pipe
+		*/
+		printf("pipe() failed");
+		exit(EXIT_FAILURE);
+	}
 
-			// Redirect STDOUT to output part of pipe 
-			dup2(pipefd[1], STDOUT_FILENO);
-			close(pipefd[0]);
-			close(pipefd[1]);
+	// Create 1st child   
+	if (fork() == 0) {
 
-			execvp(childArgv1[0], childArgv1);
-			printf("Fail to execute first command");
-			exit(EXIT_FAILURE);
-		}
-
-		// Tao tien trinh con thu hai(child 2)
-		if (fork() == 0) {
-
-			// Redirect STDIN to input part of pipe
-			dup2(pipefd[0], STDIN_FILENO);
-			close(pipefd[1]);
-			close(pipefd[0]);
-
-			execvp(childArgv2[0], childArgv2);
-			printf("Fail to execute second command");
-			exit(EXIT_FAILURE);
-		}
-
+		// Redirect STDOUT to output part of pipe 
+		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[0]);
 		close(pipefd[1]);
-		// Doi child 1 
-		wait(0);
-		// Doi child 2
-		wait(0);
+
+		execvp(childArgv1[0], childArgv1);
+		printf("Fail to execute first command");
+		exit(EXIT_FAILURE);
 	}
 
-	int main() {
-		bool running = true;
-		pid_t pid;
-		int status = 0, historyIndex = 0, waitFlag;
-		char cmdString[MAX_LINE_LENGTH];
-		char *argv[BUF_SIZE], *redirArgv[REDIR_SIZE];
-		char *childArgv1[PIPE_SIZE], *childArgv2[PIPE_SIZE];
-		char *historyList[MAX_HISTORY_SIZE];
+	// Create 2nd child
+	if (fork() == 0) {
 
-		for (int i = 0; i < MAX_HISTORY_SIZE; i++) {
-			historyList[i] = (char*)malloc(MAX_COMMAND_NAME);
-		}
+		// Redirect STDIN to input part of pipe
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[1]);
+		close(pipefd[0]);
 
-		while (running) {
-			printf("osh>");
-			fflush(stdout);
-
-			// Doc va tach, chuyen doi user command
-			while (fgets(cmdString, MAX_LINE_LENGTH, stdin) == NULL) {
-				printf("Cannot read user input!");
-				fflush(stdin);
-			}
-
-			// Bo di ki tu ket thuc chuoi
-			cmdString[strcspn(cmdString, "\n")] = '\0';
-
-			// Kiem tra xem user co nham command: "exit" khong?
-			if (strcmp(cmdString, "exit") == 0) {
-				running = false;
-				continue;
-			}
-
-			// Kiem tra xem user co nham command: "!!" ?
-			if (strcmp(cmdString, "!!") == 0) {
-				if (historyIndex == 0) {
-					fprintf(stderr, "No commands in history before\n");
-					continue;
-				}
-				strcpy(cmdString, historyList[historyIndex - 1]);
-				printf("osh>%s\n", cmdString);
-			}
-
-			SaveHistoryList(historyList, historyIndex, cmdString);
-			ParseCmd(cmdString, argv, &waitFlag);
-			ParseRedirect(argv, redirArgv);
-
-			if (ParsePipe(argv, childArgv1, childArgv2)) {
-				ExecWithPipe(childArgv1, childArgv2);
-				continue;
-			}
-
-			// Fork child process
-			pid_t pid = fork();
-
-			// Fork tra ve 2 lan neu thanh cong: 0 - child process, > 0 - parent process
-			switch (pid) {
-			case -1:
-				printf("fork() failed!");
-				exit(EXIT_FAILURE);
-
-			case 0:     // Tien trinh con
-				ChildProcess(argv, redirArgv);
-				exit(EXIT_SUCCESS);
-
-			default:    // Tien trinh cha
-				ParentProcess(pid, waitFlag);
-			}
-
-		}
-		return 0;
+		execvp(childArgv2[0], childArgv2);
+		printf("Fail to execute second command");
+		exit(EXIT_FAILURE);
 	}
 
+	close(pipefd[0]);
+	close(pipefd[1]);
+	// Wait for child 1
+	wait(0);
+	// Wait for child 2
+	wait(0);
+}
 
-	// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-	// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+	bool running = true;
+	pid_t pid;
+	int status = 0, historyIndex = 0, waitFlag;
+	char cmdString[MAX_LINE_LENGTH];
+	char *argv[BUF_SIZE], *redirArgv[REDIR_SIZE];
+	char *childArgv1[PIPE_SIZE], *childArgv2[PIPE_SIZE];
+	char *historyList[MAX_HISTORY_SIZE];
 
-	// Tips for Getting Started: 
-	//   1. Use the Solution Explorer window to add/manage files
-	//   2. Use the Team Explorer window to connect to source control
-	//   3. Use the Output window to see build output and other messages
-	//   4. Use the Error List window to view errors
-	//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-	//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 0; i < MAX_HISTORY_SIZE; i++) {
+		historyList[i] = (char*)malloc(MAX_COMMAND_NAME);
+	}
+
+	while (running) {
+		printf("osh>");
+		fflush(stdout);
+
+		// Read and parse user input
+		while (fgets(cmdString, MAX_LINE_LENGTH, stdin) == NULL) {
+			printf("Cannot read user input!");
+			fflush(stdin);
+		}
+
+		// Remove trailing endline character
+		cmdString[strcspn(cmdString, "\n")] = '\0';
+
+		// Check if user entered "exit"
+		if (strcmp(cmdString, "exit") == 0) {
+			running = false;
+			continue;
+		}
+
+		// Check if user entered "!!"
+		if (strcmp(cmdString, "!!") == 0) {
+			if (historyIndex == 0) {
+				fprintf(stderr, "No commands in history before\n");
+				continue;
+			}
+			strcpy(cmdString, historyList[historyIndex - 1]);
+			printf("osh>%s\n", cmdString);
+		}
+
+		SaveHistoryList(historyList, historyIndex, cmdString);
+		ParseCmd(cmdString, argv, &waitFlag);
+		ParseRedirect(argv, redirArgv);
+
+		if (ParsePipe(argv, childArgv1, childArgv2)) {
+			ExecWithPipe(childArgv1, childArgv2);
+			continue;
+		}
+
+		// Fork child process
+		pid_t pid = fork();
+
+		// Fork return twice on success: 0 - child process, > 0 - parent process
+		switch (pid) {
+		case -1:
+			printf("fork() failed!");
+			exit(EXIT_FAILURE);
+
+		case 0:     // In child process
+			ChildProcess(argv, redirArgv);
+			exit(EXIT_SUCCESS);
+
+		default:    // In parent process
+			ParentProcess(pid, waitFlag);
+		}
+
+	}
+	return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
